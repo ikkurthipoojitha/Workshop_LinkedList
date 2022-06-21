bcd3f651faf064d41513be95454dcfe37e016eda
ikkurthipoojitha <ipoojitha1997@gmail.com>
2022-06-21 09:03:03 +0530

    main added

---
+++ src/com/bridgelabz/Workshop/LinkedListMain.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.Workshop;
+
+public class LinkedListMain {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        linkedList.add(12);
+        linkedList.add(24);
+        linkedList.add(36);
+        linkedList.print();
+
+        System.out.println();
+        linkedList.insert(24,32);
+        linkedList.print();
+
+    }
+}
